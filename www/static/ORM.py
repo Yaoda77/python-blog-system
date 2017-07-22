32c78d341dbe55f91ec7ca4239cdb7b3ddc955a0
Yaoda <yaoda7757@qq.com>
2017-07-22 16:39:45 +0100

    2nd version of ORM.py

--- www/static/ORM.py
+++ www/static/ORM.py
@@ -33,12 +33,18 @@ async def select(sql, args, size=None):
         logging.info('rows returned: %s' %len(rs))
         return rs
 
+def create_args_string(num):
+    L = []
+    for n in range(num):
+         L.append('?')
+    return ', '.join(L)
+
 #define function to execute insert, update and delete for SQL
 async def execute(sql, args):
     log(sql)
     async with __pool.get() as conn:
         try:
-            cur = =await conn.cursor()
+            cur = await conn.cursor()
             await cur.execute(sql.replace('?', '%s'), args)
             affected = cur.rowcount
             await cur.close()
@@ -46,13 +52,66 @@ async def execute(sql, args):
             raise
         return affected
 
-from orm import Model, StringField, IntegerField
+class Field(object):
 
-class User(Model):
-    __table__ = 'users'
-    #the structure of table:
-    id = IntegerField(primary_key=True)
-    name = StringField()
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s, %s:%s>' %(self.__class__.__name__,self.column_type, self.name)
+
+class StringField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+
+class IntegerField(Field):
+
+    def __init__(self, name=None, primary_key=True, default=None, ddl='bigint'):
+        super().__init__(name, ddl, primary_key, default)
+
+class ModelMetaclass(type):
+
+    def __new__(cls, name, bases, attrs):
+        #except Model class itself:
+        if name=='Model':
+            return type.__new__(cls, name, bases, attrs)
+        #get the name of table:
+        tableName = attrs.get('__table__', None) or name
+        logging.info('found model： %s（table: %s）' %(name, tableName))
+        #get all Field and primaryKeyName:
+        mappings = dict()
+        fields = []
+        primaryKey = None
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                logging.info('found mapping: %s ==> %s' % (k, v))
+                mappings[k] = v
+                if v.primary_key:
+                    if primaryKey:
+                        raise RuntimeError('Duplicate primary key for field: %s' %k)
+                    primaryKey = k
+                else:
+                    fields.append(k)
+        if not primaryKey:
+            raise RuntimeError('Primary key not found.')
+        for k in mappings.keys():
+            attrs.pop(k)
+        escaped_fields = list(map(lambda f:'`%s`' %f, fields))
+        attrs['__mappings__'] = mappings
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey
+        attrs['__fields__'] = fields
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (
+        tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (
+        tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
 
 class Model(dict, metaclass=ModelMetaclass):
 
@@ -81,27 +140,48 @@ class Model(dict, metaclass=ModelMetaclass):
                 setattr(self, key, value)
             return value
 
-class Field(object):
+    @classmethod
+    async def find(cls, pk):
+        """find object by primary key."""
+        rs = await select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+        if len(rs) == 0:
+            return None
+        return cls(**rs[0])
 
-    def __init__(selfself, name, column_type, primary_key, default):
-        self.name = name
-        self.column_type = column_type
-        self.primary_key = primary_key
-        self.default = default
+    @classmethod
+    async def findAll(cls, require):
+        """find object by input requirement"""
+        rs = await select('%s where %s' % (cls.__select__, require), ())
+        if len(rs) == 0:
+            return None
+        return cls(**rs)
 
-    def __str__(self):
-        return '<%s, %s:%s>' %(self.__class__.__name__,self.column_type, self.name)
+    @classmethod
+    async def findNumber(cls, require):
+        """find object by input requirement and return int"""
+        rs = await select('%s where %s' % (cls.__select__, require), ())
+        if len(rs) == 0:
+            return None
+        return cls(**rs)
 
-class StringField(Field):
+    async def save(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = await execute(self.__insert__, args)
+        if rows != 1:
+            logging.warning('failed to insert record: affected rows:%s' % rows)
 
-    def __init__(self, name=None, primary_key=False, default=None):
-        super().__init__(name, ddl, primary_key, default)
+    async def update(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = await execute(self.__update__, args)
+        if rows != 1:
+            logging.warning('failed to update record: affected rows:%s' % rows)
 
-class ModelMetaclass(type):
+    async def remove(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = await execute(self.__delete__, args)
+        if rows != 1:
+            logging.warning('failed to delete record: affected rows:%s' % rows)
 
-    def __new__(cls, name, bases, attrs):
-        #except Model class itself:
-        if name=='Model':
-            return type.__new__(cls, name, bases, attrs)
-        #get the name of table:
-        tableName = attrs.get('__table__', None) or name
